e31e89b99cf31b8b7704e9ea88bff6a805ba508c
Jason Gaylord <jason@jasongaylord.com>
2024-09-14 08:24:06 -0400

    Added argument support.

--- asbuild/Program.cs
+++ asbuild/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No arguments provided.");
+                return;
+            }
+
+            Console.WriteLine("Arguments provided:");
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+
+            // Example: Handling specific arguments
+            if (args.Length >= 2 && args[0] == "--name")
+            {
+                string name = args[1];
+                Console.WriteLine($"Hello, {name}!");
+            }
+        }
+    }
+}
+
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
